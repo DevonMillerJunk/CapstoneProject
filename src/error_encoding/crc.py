8824623bfb37db853c1910a639017a8d4a902188
DevonMillerJunk <devonmillerjunk@gmail.com>
2022-07-13 13:33:36 -0400

    Change to temporary crc

--- src/error_encoding/crc.py
+++ src/error_encoding/crc.py
@@ -2,33 +2,34 @@ import math
 
 
 class CRC:
-    key = 0b11111000110010010001010000001010
+    key = bytearray(0b11111000110010010001010000001010)
     key_len = math.floor(math.log2(key)) + 1
 
     def __init__(self, crc_len):
         self.crc_len = crc_len
 
     def __gen_crc__(self, message):
-        result = ''
-        temp = '0'
-        r = 0
+        # result = ''
+        # temp = '0'
+        # r = 0
 
-        for i in range(len(a)):
-            if int(self.key) > int(temp):
-                result += '0'
-                temp += message[i]
-            else:
-                r = temp - self.key
-                if r == 0:
-                    temp = message[i]
-                    if (int(result) == 0):
-                        result = ''
-                    result += '1'
-                else:
-                    r = str(r).lstrip('0')
-                    result += '1'
-                    temp = r + message[i]
-        return r
+        # for i in range(len(message)):
+        #     if int(self.key) > int(temp):
+        #         result += '0'
+        #         temp += message[i]
+        #     else:
+        #         r = temp - self.key
+        #         if r == 0:
+        #             temp = message[i]
+        #             if (int(result) == 0):
+        #                 result = ''
+        #             result += '1'
+        #         else:
+        #             r = str(r).lstrip('0')
+        #             result += '1'
+        #             temp = r + message[i]
+        # return r
+        return self.key
 
     def __encode__(self, message):
         return message + self.__gen_crc__(message)
