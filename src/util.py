3e01c45ca63a6f722b815d1fea6cac9d3c02d62b
DevonMillerJunk <devonmillerjunk@gmail.com>
2022-07-17 23:11:49 -0400

    Calculate bit error rate

---
+++ src/util.py
@@ -0,0 +1,12 @@
+from bitarray import bitarray
+
+
+def BER(input1: bytes, input2: bytes) -> float:
+    array1 = bitarray()
+    array1.frombytes(input1)
+    array2 = bitarray()
+    array2.frombytes(input2)
+    if (len(array1) != len(array2)):
+        raise Exception("Unequal Lengths")
+    result = array1 ^ array2
+    return float(result.count(1)) / float(len(result))
