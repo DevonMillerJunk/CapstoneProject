981b96f0b973f9b85bc251319d47137f35c699fd
DevonMillerJunk <devonmillerjunk@gmail.com>
2023-02-22 15:11:31 -0500

    Add ack functionality to the packet definition

--- src/packetization.py
+++ src/packetization.py
@@ -15,20 +15,28 @@ class Packet:
     CRC = crc.CRC()
     USE_CRC:bool = True # Temporary while we test with/without crc
     INT_LEN:int = 16 # used for decoding, in bits
-    # Total Packet size (in bytes) = payload + packet_num + total_packets + crc = BUF_SZ
-    PACKET_DATA_SZ:int = BUF_SZ - (2 * math.ceil(INT_LEN / 8)) - (CRC.CRC_SZ if USE_CRC else 0)
+    # Total Packet size (in bytes) = payload + packet_num + total_packets + crc + is_ack = BUF_SZ
+    PACKET_DATA_SZ:int = BUF_SZ - 1 - (2 * math.ceil(INT_LEN / 8)) - (CRC.CRC_SZ if USE_CRC else 0)
     
     # Constructor
-    def __init__(self, packet_num: int, total_packets: int, payload: bytes):
-        if len(payload) > self.PACKET_DATA_SZ:
-            raise Exception("Invalid Packet Payload length")
-        if packet_num < 0 or packet_num >= total_packets:
+    # If is_ack is true, total_packets and payload are not used
+    def __init__(self, is_ack: bool, packet_num: int, total_packets: int, payload: bytes):
+        if packet_num < 0:
             raise Exception("Invalid Packet Num")
-        if total_packets < 0:
-            raise Exception("Invalid Total Packets Num")
         self.packet_num: int = packet_num
-        self.total_packets: int = total_packets # number of packets for this message
-        self.payload: bytes  = payload # Must be <= PACKET_DATA_SZ bytes in length.
+        self.is_ack: bool = is_ack # is a data packet or an ack packet
+        if is_ack == True:
+            self.total_packets = None
+            self.payload = None
+        else:
+            if packet_num >= total_packets:
+                raise Exception("Invalid Packet Num")
+            if len(payload) > self.PACKET_DATA_SZ:
+                raise Exception("Invalid Packet Payload length")
+            if total_packets < 0:
+                raise Exception("Invalid Total Packets Num")
+            self.total_packets: int = total_packets # number of packets for this message
+            self.payload: bytes  = payload # Must be <= PACKET_DATA_SZ bytes in length.
         
     # Getters
     def get_payload(self) -> bytes:
@@ -37,19 +45,20 @@ class Packet:
         return self.packet_num
     def get_total_packets(self) -> int:
         return self.total_packets
+    
+    # Byte representing is_ack
+    def __is_ack_bits(self) -> bitarray:
+        return bitarray('11111111') if self.is_ack else bitarray('00000000')
         
-    # Creates bytes for the packet num
-    def __int_to_bits(self, num: int) -> bitarray:
-        packet_num_bits:bitarray = int2ba(num)
-        padded_0s:bitarray = bitarray(self.INT_LEN - len(packet_num_bits))
-        padded_0s.setall(0)
-        return padded_0s + packet_num_bits
-        
-    # Creates bytes following: packet num | total_packets | payload
+    # Creates bytes following: is_ack | packet num | total_packets | payload
+    # or for an ack: is_ack | packet_num
     def __combined_packet(self) -> bytes:
-        payload_bits:bitarray = bitarray()
-        payload_bits.frombytes(self.payload)
-        return (self.__int_to_bits(self.packet_num) + self.__int_to_bits(self.total_packets) + payload_bits).tobytes()
+        if self.is_ack:
+            return (self.__is_ack_bits() + int2ba(self.packet_num, self.INT_LEN)).tobytes()
+        else:
+            payload_bits:bitarray = bitarray()
+            payload_bits.frombytes(self.payload)
+            return (self.__is_ack_bits() + int2ba(self.packet_num, self.INT_LEN) + int2ba(self.total_packets, self.INT_LEN) + payload_bits).tobytes()
  
     # Return the packet encoded as bytes
     def encode(self) -> bytes:
@@ -57,7 +66,7 @@ class Packet:
         if self.USE_CRC:
             return self.CRC.encode(combined_packet)
         return combined_packet
- 
+        
     # Decode a given byte array into a packet
     # Python doesn't allow multiple constructors
     @staticmethod
@@ -67,10 +76,14 @@ class Packet:
             result_bytes = Packet.CRC.decode(data)
         bits:bitarray = bitarray()
         bits.frombytes(result_bytes)
-        packet_num:int = ba2int(bits[0:Packet.INT_LEN])
-        total_packets:int = ba2int(bits[Packet.INT_LEN:2*Packet.INT_LEN])
-        payload = bits[2*Packet.INT_LEN:len(bits)].tobytes()
-        return Packet(packet_num, total_packets, payload)
+        is_ack: bool = False if ba2int(bits[0:8]) == 0 else True
+        packet_num:int = ba2int(bits[8:8+Packet.INT_LEN])
+        if is_ack == True:
+            return Packet(True, packet_num, None, None)
+        else:
+            total_packets:int = ba2int(bits[8+Packet.INT_LEN:8+2*Packet.INT_LEN])
+            payload = bits[8+2*Packet.INT_LEN:len(bits)].tobytes()
+            return Packet(False, packet_num, total_packets, payload)
 
 # Represents a payload of data that will be sent in 1+ packets
 # over the network. Can be used to store and decode incoming packets.
@@ -81,10 +94,11 @@ class Packet:
 #   3. Detect missing frames: frame.missing_packets()
 #   4. When frame.all_packets_recv() == true, can call frame.get_payload()
 class Frame:
-    def __init__(self, first_payload: bytes):
+    def __init__(self, first_packet: Packet):
         # first_packet is just the first packet received,
         # doesn't need to be logically first
-        first_packet = Packet.decode(first_payload)
+        if first_packet.is_ack == True:
+            raise Exception("Error: cannot decode frame with ACK packet")
         self.total_packets: int = first_packet.get_total_packets()
         self.packets: List[Packet] = []
         for i in range(self.total_packets):
@@ -93,17 +107,16 @@ class Frame:
             else:
                 self.packets.append(None)
     
-    # append the received payload to the frame
-    # returns the packet number of the received packet
-    def append(self, payload: bytes) -> int:
-        packet = Packet.decode(payload)
+    # append the received packet to the frame
+    def append(self, packet: Packet):
         ## TODO: keep these for testing, consider commenting out once code properly tested
+        if packet.is_ack == True:
+            raise Exception("Error: cannot decode frame with ACK packet")
         if self.packets[packet.get_packet_num()] != None:
             raise Exception("Warning: Overwritting received packet")
         if self.total_packets != packet.get_total_packets():
             raise Exception("Warning: Mismatched frame total_packets amount")
         self.packets[packet.get_packet_num()] = packet
-        return packet.get_packet_num()
       
     # determine which packets are missing  
     def missing_packets(self) -> List[int]:
@@ -130,23 +143,34 @@ class Frame:
         payload_bits.frombytes(payload)
         packets:List[Packet] = []
         bit_per_packet = Packet.PACKET_DATA_SZ * 8
+        print(f'Creating {total_packets} packets with {Packet.PACKET_DATA_SZ} bytes of data in each packet.')
         for i in range(total_packets):
-            packets.append(Packet(i, total_packets, payload_bits[i*bit_per_packet:min(len(payload_bits), (i+1)*bit_per_packet)].tobytes()))
+            packets.append(Packet(False, i, total_packets, payload_bits[i*bit_per_packet:min(len(payload_bits), (i+1)*bit_per_packet)].tobytes()))
         return packets    
    
 def packet_test1():
+    print("### RUNNING PACKET TEST ###")
+    print("Testing Data Packet:")
     input_message: str = "aosjbndoasiidboasibdoaibsdoibasoidasbljkadsnblkasnlkadsnlkas"
     encoded_input:bytes = input_message.encode()
     packet_num:int = 12089
 
-    packet = Packet(packet_num, encoded_input)
+    packet = Packet(False, packet_num, packet_num + 1, encoded_input)
     encoded_packet:bytes = packet.encode()
     decoded_packet = Packet.decode(encoded_packet)
 
-    print(packet_num == decoded_packet.packet_num)
-    print(input_message == decoded_packet.payload.decode())
+    print(f'Matching packet num decoded: {packet_num == decoded_packet.packet_num}')
+    print(f'Matching payload decoded: {input_message == decoded_packet.payload.decode()}')
+    
+    print("Testing Ack Packet:")
+    ack_packet = Packet(True, packet_num, None, None)
+    encoded_ack_packet:bytes = ack_packet.encode()
+    decoded_ack_packet = Packet.decode(encoded_ack_packet)
+    print(f'Matching packet num decoded: {packet_num == decoded_ack_packet.packet_num}')
+    print(f'Matching packet is ack: {decoded_ack_packet.is_ack}')
     
 def frame_test1():
+    print("### RUNNING FRAME TEST ###")
     input_message: str = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dignissim convallis aenean et tortor. Suspendisse sed nisi lacus sed viverra tellus. Nisl condimentum id venenatis a. Sociis natoque penatibus et magnis dis parturient. Egestas maecenas pharetra convallis posuere morbi leo urna. Pharetra et ultrices neque ornare aenean euismod elementum nisi quis. Eget felis eget nunc lobortis mattis aliquam faucibus purus in. Amet dictum sit amet justo donec enim. Consectetur libero id faucibus nisl. In metus vulputate eu scelerisque felis imperdiet proin fermentum. Orci dapibus ultrices in iaculis nunc sed augue lacus. Fusce ut placerat orci nulla pellentesque dignissim enim. Viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Sed augue lacus viverra vitae congue eu consequat ac felis.
         Enim nec dui nunc mattis enim. Scelerisque mauris pellentesque pulvinar pellentesque. Adipiscing bibendum est ultricies integer quis. A diam sollicitudin tempor id eu nisl nunc mi. Purus ut faucibus pulvinar elementum integer enim neque. Mauris augue neque gravida in fermentum et sollicitudin. Aliquam nulla facilisi cras fermentum odio. Pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna. In fermentum posuere urna nec tincidunt praesent semper feugiat. Aenean pharetra magna ac placerat. Aliquam vestibulum morbi blandit cursus risus at. Vitae semper quis lectus nulla at volutpat diam ut. Ultrices vitae auctor eu augue ut. Egestas purus viverra accumsan in nisl nisi scelerisque. Cursus risus at ultrices mi tempus imperdiet nulla. Mattis enim ut tellus elementum sagittis vitae.
         Erat velit scelerisque in dictum non. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt. Adipiscing elit pellentesque habitant morbi tristique senectus et netus. Dui faucibus in ornare quam viverra orci. Enim neque volutpat ac tincidunt vitae semper quis lectus. Aliquet nec ullamcorper sit amet risus nullam eget felis. Libero nunc consequat interdum varius sit. Sit amet cursus sit amet dictum sit amet. A diam sollicitudin tempor id eu nisl nunc mi ipsum. Sed vulputate mi sit amet mauris commodo quis imperdiet massa. Mauris commodo quis imperdiet massa tincidunt nunc. In aliquam sem fringilla ut morbi tincidunt augue interdum. Nulla at volutpat diam ut. Tortor posuere ac ut consequat semper viverra nam libero. Tellus at urna condimentum mattis. Congue quisque egestas diam in.
@@ -160,13 +184,13 @@ def frame_test1():
     # Shuffle the list to ensure correctness under misordered receiving
     random.shuffle(frames_to_send)
     
-    recv_frame: Frame = Frame(frames_to_send[0].encode())
+    recv_frame: Frame = Frame(frames_to_send[0])
     for i in range(1, len(frames_to_send)):
-        recv_frame.append(frames_to_send[i].encode())
+        recv_frame.append(frames_to_send[i])
     if len(recv_frame.missing_packets()) != 0 or recv_frame.all_packets_recv() == False:
         print("Still missing packets")
     decoded_message: str = recv_frame.get_payload().decode()
-    
     print(f'Returned message equal? {decoded_message == input_message}')
-   
-#frame_test1()
+
+packet_test1()
+frame_test1()
