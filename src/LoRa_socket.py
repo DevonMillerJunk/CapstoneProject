32d0ebf1307537b4f59fb2f7dddc8baef5516d1b
Braden Bakker <43456666+bradenbakker@users.noreply.github.com>
2023-02-23 13:36:09 -0500

    Batch sends and refactor for packet class (#11)
    * add batch send function to send and ack multiple packets
    refactor to use packet class for encoding/decoding
    
    * Modify to work with packetization code
    
    * enable full frame recieve
    
    ---------
    
    Co-authored-by: DevonMillerJunk <devonmillerjunk@gmail.com>

--- src/LoRa_socket.py
+++ src/LoRa_socket.py
@@ -1,12 +1,13 @@
 # This file is used for LoRa and Raspberry pi4B related issues
 
+from typing import Tuple
 import RPi.GPIO as GPIO
 import serial
 import time
 import constants
 import settings as s
-import error_encoding.crc as crc
 import util as u
+from packetization import Packet, Frame
 
 
 class LoRa_socket:
@@ -29,7 +30,7 @@ class LoRa_socket:
     ser = None
     connected_address = 0
     connected_freq = offset_freq
-    packet_num = 0
+    max_retries = 10
 
     def __init__(self,serial_num=constants.SERIAL_NUM,freq=constants.FREQ,\
                  addr=0,power=constants.POWER,rssi=s.RSSI,air_speed=constants.AIR_SPEED,\
@@ -37,11 +38,9 @@ class LoRa_socket:
                  relay=False,lbt=False,wor=False):
         self.rssi = rssi
         self.addr = addr
-        self.packet_num = 0
         self.freq = freq
         self.serial_n = serial_num
         self.power = power
-        self.crc = crc.CRC()
         print(self.power, air_speed, buffer_size)
         # Initial the GPIO for M0 and M1 Pin
         GPIO.setmode(GPIO.BCM)
@@ -56,7 +55,6 @@ class LoRa_socket:
         self.ser.flushInput()
         self.set(freq, addr, power, rssi, air_speed, net_id, buffer_size,
                  crypt, relay, lbt, wor)
-
 
     def set(self,freq,addr,power,rssi,air_speed,\
             net_id=0,buffer_size=constants.BUF_SZ,crypt=0,\
@@ -172,132 +170,161 @@ class LoRa_socket:
             print("Power is {0} dBm" +
                   constants.LORA_POWER_DIC.get(None, power_temp))
             GPIO.output(self.M1, GPIO.LOW)
+        GPIO.output(self.M1, GPIO.LOW)
+        GPIO.output(self.M0, GPIO.LOW)
+        time.sleep(0.1)
 
     def __format_addr__(self, address: int) -> bytes:
         return bytes([address >> 8]) + bytes([address & 0xff])
 
-    def __encode_data__(self, payload: str) -> bytes:
-        encoding: bytes = payload.encode()
-        length = len(encoding)
-        return bytes([length]) + encoding
-
+    def __encode_data__(self, packet: Packet) -> bytes:
+        encoding: bytes = packet.encode()
+        return bytes([len(encoding)]) + encoding
+    
+    def __raw_send(self, data: bytes):
+        self.ser.write(data)
+        # if self.rssi == True:
+        # self.__get_channel_rssi()
+        # TODO: test for removal
+        time.sleep(0.05)
+        
     # the sending message format
     #
     # receiving node         receiving node       receiving node      own high 8bit     own low 8bit         own           message
     # high 8bit address      low 8bit address       frequency           address           address          frequency       payload
-    def __send_packet(self, address: int, rec_freq: int, payload):
+    def __send_packet(self, address: int, packet: Packet) -> None:
+        # TODO: remove to speed up sending
         print("Sending Packet to address" + str(address) + " from address " +
               str(self.addr))
         data: bytes = self.__format_addr__(address) +\
             bytes([self.offset_freq]) +\
             self.__format_addr__(self.addr) +\
             bytes([self.offset_freq]) +\
-            self.__encode_data__(payload)
+            self.__encode_data__(packet)
         self.__raw_send(data)
-
-    def send(self, payload, address: int=connected_address, rec_freq: int=connected_freq):
-        retries = 0
-        response = None
-        while response is None and retries <= 10:
-            self.__send_packet(address, rec_freq, payload)
-            ack_delay = 4 if self.rssi else 1
-            (response, _, _, _, _) = self.__receive(ack_delay)
-            if not response:
-                retries += 1
-        if response is not None:
-            self.packet_num = int(response)
-        else:
-            print("packet delivery failed for " + str(self.packet_num))
-
-    def broadcast(self, payload):
+        
+    def __broadcast_packet(self, packet: Packet) -> None:
+        # TODO: remove to speed up sending
+        print("Broadcasting Packet")
         data: bytes = bytes([255]) +\
-                      bytes([255]) +\
-                      bytes([self.offset_freq]) +\
-                      bytes([255]) +\
-                      bytes([255]) +\
-                      bytes([self.offset_freq]) +\
-                      payload
+            bytes([255]) +\
+            bytes([self.offset_freq]) +\
+            bytes([255]) +\
+            bytes([255]) +\
+            bytes([self.offset_freq]) +\
+            self.__encode_data__(packet)
         self.__raw_send(data)
 
-    def __raw_send(self, data):
-        GPIO.output(self.M1, GPIO.LOW)
-        GPIO.output(self.M0, GPIO.LOW)
-        time.sleep(0.1)
-
-        self.ser.write(data)
-        # if self.rssi == True:
-        # self.__get_channel_rssi()
-        time.sleep(0.1)
-
-    def __send_ack(self):
-        print("Sending Ack to address:" + str(self.connected_address) +
-              " and freq: " + str(self.connected_freq))
-        self.packet_num += 1
-        data: bytes = self.__format_addr__(self.connected_address) +\
-               bytes([self.connected_freq]) +\
-               self.__format_addr__(self.addr) +\
-               bytes([self.offset_freq]) +\
-               self.__encode_data__(str(self.packet_num))
-        self.__raw_send(data)
-
-    def __receive(self, timeout: float = 1):
-        check_period = 0.01
+    def __send_ack(self, packet_num: int, address: int=connected_address) -> None:
+        self.__send_packet(address, Packet(True, packet_num, None, None))
+        
+    def send(self, payload: bytes, address: int=connected_address) -> None:
+        # Packetize input
+        packets: list[Packet] = Frame.packetize(payload)
+        
+        # Send Packets
+        unacked_packets = set()
+        for packet in packets:
+            unacked_packets.add(packet.packet_num)
+        retries = 0
+        while len(unacked_packets) > 0 and retries <= self.max_retries:
+            # Send all un_acked packets
+            for packet in packets:
+                if packet.packet_num in unacked_packets:
+                    self.__send_packet(address, packet)
+            
+            # Remove all acks from buffer
+            while len(unacked_packets) > 0:
+                (response, _, _, _, _) = self.__receive(4 if self.rssi else 1)
+                if response is not None and response.is_ack == True:
+                    unacked_packets.remove(response.packet_num)
+                else:
+                    break
+            retries += 1
+        if len(unacked_packets) > 0:
+            print("packet delivery failed for " + str(unacked_packets))
+        else:
+            # TODO: remove for speedup
+            print(f'Payload delivered. {retries} retries occurred.')
+        
+    # Returns packet, address, freq, pkt_rssi, channel_rssi (rssi are None if self.rssi == False)
+    def __receive(self, timeout: float = 1) -> Tuple[Packet | None, int | None, int | None, int | None, int | None]:
+        check_period = 0.005
         if (timeout > 0):
             curr_time: float = 0
             while self.ser.inWaiting() <= 0 and curr_time < timeout:
                 time.sleep(check_period)
                 curr_time += check_period
         if self.ser.inWaiting() > 0:
-            time.sleep(0.5)
+            # TODO: remove after testing
+            time.sleep(0.05)
+            
+            #TODO: may potentially require a loop while readeding from ser
+            # in the case that part of the message arrived
             r_buff: bytes = self.ser.read(self.ser.inWaiting())
             address = int.from_bytes(r_buff[0:2], "big")
             freq = r_buff[2]
             msg_len = r_buff[3]
-            msg = (r_buff[4:min(len(r_buff), 4 + msg_len)]).decode(
-            )  #Note: should change to be a wait for the len to arrive
-            #decoded_msg = self.crc.decode(bytes(msg))
+            msg = r_buff[4:min(len(r_buff), 4 + msg_len)]
+            packet = Packet.decode(msg)
+            pkt_rssi = None
+            channel_rssi = None
 
+            # TODO: remove for speedup
             print(
                 "receive message from node address with frequency\033[1;32m %d,%d.125MHz\033[0m"
                 % (address, self.start_freq + freq),
                 end='\r\n',
                 flush=True)
-            print("message is " + msg, end='\r\n')
 
             # print the rssi
             if self.rssi:
-                # print('\x1b[3A',end='\r')
                 pkt_rssi = (256 - r_buff[-1:][0])*-1
-                print("the packet rssi value: -{0}dBm".format(256 - r_buff[-1:][0]))
                 channel_rssi = self.__get_channel_rssi()
+                print(f'the packet rssi value: -{pkt_rssi}dBm, channel rssi value: -{channel_rssi}dBm')
                 
-                return (msg, address, freq, pkt_rssi, channel_rssi)
-            else:
-                pass
-                #print('\x1b[2A',end='\r')
-            return (msg, address, freq, None, None)
+            return (packet, address, freq, pkt_rssi, channel_rssi)
         else:
             return (None, None, None, None, None)
 
-    def recv(self, timeout: float = 1):
-        (res, addr, freq, pkt_rssi, ch_rssi) = self.__receive(timeout)
-        self.connected_address = addr
-        self.connected_freq = freq
+    # Receives one frame (in bytes)
+    # Returns: payload, address
+    # Note: timeout is between consecutive packets, could take multiples of the timeout
+    def recv(self, timeout: float = 1) -> Tuple[bytes, int] | None:
+        (res, addr, freq, _, _) = self.__receive(timeout)
         if (res != None):
-            self.__send_ack()
-        return (res, addr, freq, pkt_rssi, ch_rssi)
+            self.connected_address = addr
+            self.connected_freq = freq
+            self.__send_ack(res.packet_num)
+            frame: Frame = Frame(res)
+            while frame.all_packets_recv() == False:
+                (res, addr, _, _, _) = self.__receive(timeout)
+                if res != None and addr == self.connected_address and res.is_ack == False:
+                    frame.append(res)
+                else:
+                    # Couldn't retrieve package in timeout, exiting
+                    print(f'Unable to receive full package in timeout. {frame.missing_packets()} not received')
+                    break
+            return (frame.get_payload(), self.connected_address) if frame.all_packets_recv() else None
+        return None
 
     def connect(self):
-        retries = 10 
         retryPeriod = 5  #seconds
+        payload: str = str(self.addr) + "," + str(self.offset_freq)
+        packet = Packet(False, 0, 1, payload.encode())
+        
         curr_retry = 0
         response = None
-        payload: str = str(self.addr) + "," + str(self.offset_freq)
-        while response is None and curr_retry < retries:
-            self.broadcast(payload)
-            (response, addr, freq, _, _) = self.__receive(retryPeriod)
-            if not response:
+        addr = None
+        freq = None
+        while curr_retry <= self.max_retries:
+            self.__broadcast_packet(packet)
+            (res, addr, freq, _, _) = self.__receive(retryPeriod)
+            if not response or response.is_ack == False or response.packet_num != packet.packet_num:
                 curr_retry += 1
+            else:
+                response = res
+                break
         if response is not None:
             self.connected_address = addr
             self.connected_freq = freq
@@ -308,14 +335,12 @@ class LoRa_socket:
 
     def accept(self):
         listen = None
-        while listen == None:
+        while listen == None or listen.is_ack == False:
             (listen, _, _, _, _) = self.__receive()
-        resp = listen.split(",")
-        self.connected_address = resp[0]
-        self.connected_freq = resp[1]
-        payload: str = str(self.addr) + "," + str(self.offset_freq)
-        self.__send_packet(self.connected_address, self.connected_freq,
-                           payload)
+        data = listen.payload.split(",")
+        self.connected_address = data[0]
+        self.connected_freq = data[1]
+        self.__send_ack(listen.packet_num)
         print("accepted connection request from" + self.connected_address +
               ", " + self.connected_freq)
 
